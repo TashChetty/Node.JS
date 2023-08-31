647ac63535fae859ab6841a1e895c6cbd3b353c5
TashChetty <natasha.chetty2@gmail.com>
2023-08-31 19:00:31 +0200

    Lesson 23

---
+++ Lesson 23/recipe_app_3/controllers/usersController.js
@@ -0,0 +1,184 @@
+"use strict";
+
+const User = require("../models/user"),
+  getUserParams = body => {
+    return {
+      name: {
+        first: body.first,
+        last: body.last
+      },
+      email: body.email,
+      password: body.password,
+      zipCode: body.zipCode
+    };
+  };
+
+module.exports = {
+  index: (req, res, next) => {
+    User.find()
+      .then(users => {
+        res.locals.users = users;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching users: ${error.message}`);
+        next(error);
+      });
+  },
+  indexView: (req, res) => {
+    res.render("users/index", {
+      flashMessages: {
+        success: "Loaded all users!"
+      }
+    });
+  },
+  new: (req, res) => {
+    res.render("users/new");
+  },
+  create: (req, res, next) => {
+    if (req.skip) return next();
+    let userParams = getUserParams(req.body);
+    User.create(userParams)
+      .then(user => {
+        req.flash("success", `${user.fullName}'s account created successfully!`);
+        res.locals.redirect = "/users";
+        res.locals.user = user;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error saving user: ${error.message}`);
+        res.locals.redirect = "/users/new";
+        req.flash("error", `Failed to create user account because: ${error.message}.`);
+        next();
+      });
+  },
+  redirectView: (req, res, next) => {
+    let redirectPath = res.locals.redirect;
+    if (redirectPath) res.redirect(redirectPath);
+    else next();
+  },
+  show: (req, res, next) => {
+    let userId = req.params.id;
+    User.findById(userId)
+      .then(user => {
+        res.locals.user = user;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching user by ID: ${error.message}`);
+        next(error);
+      });
+  },
+  showView: (req, res) => {
+    res.render("users/show");
+  },
+  edit: (req, res, next) => {
+    let userId = req.params.id;
+    User.findById(userId)
+      .then(user => {
+        res.render("users/edit", {
+          user: user
+        });
+      })
+      .catch(error => {
+        console.log(`Error fetching user by ID: ${error.message}`);
+        next(error);
+      });
+  },
+  update: (req, res, next) => {
+    let userId = req.params.id,
+      userParams = {
+        name: {
+          first: req.body.first,
+          last: req.body.last
+        },
+        email: req.body.email,
+        password: req.body.password,
+        zipCode: req.body.zipCode
+      };
+    User.findByIdAndUpdate(userId, {
+      $set: userParams
+    })
+      .then(user => {
+        res.locals.redirect = `/users/${userId}`;
+        res.locals.user = user;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error updating user by ID: ${error.message}`);
+        next(error);
+      });
+  },
+  delete: (req, res, next) => {
+    let userId = req.params.id;
+    User.findByIdAndRemove(userId)
+      .then(() => {
+        res.locals.redirect = "/users";
+        next();
+      })
+      .catch(error => {
+        console.log(`Error deleting user by ID: ${error.message}`);
+        next();
+      });
+  },
+  login: (req, res) => {  //add the login action
+    res.render("users/login");
+  },
+  authenticate: (req, res, next) => { //add the authenticate action
+    User.findOne({ email: req.body.email }) //query for one user by email
+      .then(user => {
+        if (user) {                         //check whether a user is found
+          user.passwordComparison(req.body.password).then(passwordsMatch => { //call the password comparison method on the User model.
+            if (passwordsMatch) {                                              // check whether the passwords match
+              res.locals.redirect = `/users/${user._id}`;
+              req.flash("success", `${user.fullName}'s logged in successfully!`);
+              res.locals.user = user;
+            } else {
+              req.flash("error", "Failed to log in user account: Incorrect Password.");
+              res.locals.redirect = "/users/login";
+            }
+            next();                                                             //call the nexr middelware function with redirect path and flash message set
+          });
+        } else {
+          req.flash("error", "Failed to log in user account: User account not found.");
+          res.locals.redirect = "/users/login";
+          next();
+        }
+      })
+      .catch(error => { //log errors to the console and pass to the next middleware error handler
+        console.log(`Error logging in user: ${error.message}`);
+        next(error);
+      });
+  },
+  validate: (req, res, next) => { //add the validate function
+    req
+      .sanitizeBody("email")
+      .normalizeEmail({
+        all_lowercase: true
+      })
+      .trim();                  //remove whitespace with the trim method
+    req.check("email", "Email is invalid").isEmail();
+    req
+      .check("zipCode", "Zip code is invalid")
+      .notEmpty()
+      .isInt()
+      .isLength({
+        min: 5,
+        max: 5
+      })
+      .equals(req.body.zipCode);  //validate the zipcode field
+    req.check("password", "Password cannot be empty").notEmpty(); //validate the password field
+
+    req.getValidationResult().then(error => { //get results of previous validations
+      if (!error.isEmpty()) {
+        let messages = error.array().map(e => e.msg);
+        req.skip = true;                    //set skip property to true
+        req.flash("error", messages.join(" and ")); //add error messages as flash messages
+        res.locals.redirect = "/users/new"; //set redirect path for the new view
+        next();   //call the next middelware function
+      } else {
+        next();
+      }
+    });
+  }
+};
